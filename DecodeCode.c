813092af2a53cc7ad90adc222970afbc938f8cef
Jairo Bedolla <jairobedolla@ucsb.edu>
2019-11-08 19:13:51 -0800

    second

---
+++ DecodeCode.c
@@ -0,0 +1,24 @@
+#include "DecodeCode.h"
+
+
+mipsinstruction decode(int value)
+{
+	mipsinstruction instr;
+
+	// TODO: fill in the fields
+	instr.funct = value & 0x3F;
+	if ((value & (1 << 15)) >> 15){
+		instr.immediate = ((value & 0xFFFF) | 0xFFFF0000)
+	}	
+	else{
+		instr.immediate = value & 0xFFFF;
+	}
+	instr.rd = (value & (0x1F << 11)) >> 11;
+	instr.rt = (value & (0x1F << 16)) >> 16;
+	instr.rs = (value & (0x1F << 21)) >> 22;
+	instr.opcode = (value & (0x3F << 26)) >> 26;
+
+	return instr;
+}
+
+
---
+++ functions.asm
@@ -0,0 +1,148 @@
+# xSpim Memory Demo Program
+
+#  Data Area
+.data
+
+space:
+    .asciiz " "
+
+newline:
+    .asciiz "\n"
+
+dispArray:
+    .asciiz "\nCurrent Array:\n"
+
+convention:
+    .asciiz "\nConvention Check\n"
+
+myArray:
+	.word 0 33 123 -66 332 -1 -223 453 9 45 -78 -14  
+
+#Text Area (i.e. instructions)
+.text
+
+main:
+    ori     $v0, $0, 4          
+    la      $a0, dispArray 
+    syscall
+
+    ori     $s1, $0, 12
+    la      $s0, myArray
+
+    add     $a1, $0, $s1
+    add     $a0, $0, $s0
+ 
+    jal     DispArray
+
+    ori     $s2, $0, 0
+    ori     $s3, $0, 0
+    ori     $s4, $0, 0
+    ori     $s5, $0, 0
+    ori     $s6, $0, 0
+    ori     $s7, $0, 0
+    
+    add     $a1, $0, $s1
+    add     $a0, $0, $s0
+
+    jal     PrintReverse
+
+    add     $s1, $s1, $s2
+    add     $s1, $s1, $s3
+    add     $s1, $s1, $s4
+    add     $s1, $s1, $s5
+    add     $s1, $s1, $s6
+    add     $s1, $s1, $s7
+
+    add     $a1, $0, $s1
+    add     $a0, $0, $s0
+    jal     DispArray
+
+    j       Exit
+
+DispArray:
+    addi    $t0, $0, 0 
+    add     $t1, $0, $a0
+
+dispLoop:
+    beq     $t0, $a1, dispend
+    sll     $t2, $t0, 2
+    add     $t3, $t1, $t2
+    lw      $t4, 0($t3)
+
+    ori     $v0, $0, 1
+    add     $a0, $0, $t4
+    syscall
+
+    ori     $v0, $0, 4
+    la      $a0, space
+    syscall
+
+    addi    $t0, $t0, 1
+    j       dispLoop    
+
+dispend:
+    ori     $v0, $0, 4
+    la      $a0, newline
+    syscall
+    jr      $ra 
+
+ConventionCheck:
+    addi    $t0, $0, -1
+    addi    $t1, $0, -1
+    addi    $t2, $0, -1
+    addi    $t3, $0, -1
+    addi    $t4, $0, -1
+    addi    $t5, $0, -1
+    addi    $t6, $0, -1
+    addi    $t7, $0, -1
+    ori     $v0, $0, 4
+    la      $a0, convention
+    syscall
+    addi $v0, $zero, -1
+    addi $v1, $zero, -1
+    addi $a0, $zero, -1
+    addi $a1, $zero, -1
+    addi $a2, $zero, -1
+    addi $a3, $zero, -1
+    addi $k0, $zero, -1
+    addi $k1, $zero, -1
+    jr      $ra
+    
+Exit:
+    ori     $v0, $0, 10
+    syscall
+
+# COPYFROMHERE - DO NOT REMOVE THIS LINE
+
+PrintReverse:
+    #TODO: write your code here, $a0 stores the address of the array, $a1 stores the length of the array
+	
+	beq $a1, $zero, jump_back		#when len = 0, exit the functiont
+
+	addi $sp, $sp, -4			# move $s0 and $ra into the stack
+	sw $ra, 0($sp)
+	
+	sll $t0, $a1, 2 			# mult pos * 4 to get offset
+	add $t1, $a0, $t0			# add offset to the starting adress
+
+	lw $t2, 0($t1)				# put word at the last adress in $t2 
+
+	move $t3, $a0				# store value in a0 into temp
+
+        li $v0, 1				# print the value $t2
+        move $a0, $t2
+	syscall
+
+	jal ConventionCheck	
+
+	move $a0, $t3				# restore $a0	
+	addi $a1, $a1, -1			# len = len-1
+
+	jal PrintReverse
+
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4                        # restore the stack	
+	
+		
+    # Do not remove this linei
+	jr      $ra
